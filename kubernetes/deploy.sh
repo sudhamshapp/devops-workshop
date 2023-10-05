2ed1a4c9744bf9052ed5d4b9dcff07c997cc3bf2
sudhamshapp <sudhamsh1996@gmail.com>
2023-10-05 13:38:56 +0000

    added the manifests from the server

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-kubectl apply -f namespace.yaml
-kubectl apply -f secret.yaml
-kubectl apply -f deployment.yaml
-kubectl apply -f service.yaml
+kubectl apply -f namespace.yml
+kubectl apply -f secret.yml
+kubectl apply -f deployment.yml
+kubectl apply -f service.yml
